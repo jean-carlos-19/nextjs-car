252b9a6e65c33e50d2cb839735bada1140838663
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-15 12:09:22 -0500

    actualizando los estilos con el linter de prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
+
+import { Hero } from "@/atomic/component";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      
+      <Hero />
     </main>
-  )
+  );
 }
