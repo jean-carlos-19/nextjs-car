da2b8fb72557d0165cd7ff3e036aa827fd634bc8
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-15 13:57:20 -0500

    Modified CustomButton file: Built the CustomButton element

--- atomic/element/CustomButton.tsx
+++ atomic/element/CustomButton.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+"use client";
 
-const CustomButton = () => {
+import React from 'react'
+import {CustomButtonProps} from '@/types'
+
+const CustomButton = (props:CustomButtonProps) => {
+    const {title,containerStyles,handleClick} = props;
   return (
-    <div>
-      
-    </div>
+    <button
+        disabled={false}
+        type={"button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}
+    >
+      <span className="flex-1">
+        {title}
+      </span>
+    </button>
   )
 }
 
