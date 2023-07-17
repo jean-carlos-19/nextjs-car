1d26ab95244d851595aeb56d4b5ad5090effa64c
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-17 09:56:33 -0500

    Modified useHome.ts file: built useHome hooks ready to use.

--- src/app/hooks/useHome.ts
+++ src/app/hooks/useHome.ts
@@ -1,4 +1,28 @@
-const useHome =()=>{
+import { useCallback, useEffect, useState } from 'react'
+import { SearchService } from '@/services'
 
+const useHome =()=>{
+    const service: SearchService = SearchService.getService();
+    const [isDataEmpty, setDataEmpty] = useState(false);
+    const [data,setData]=useState([])
+  
+    useEffect(() => {
+      searchCar();
+    }, [])
+  
+    const searchCar = useCallback(async () => {
+      try {
+        const rs = await service.search(new URLSearchParams([["model", "corola"]]));
+        const { data } = rs;
+        const empty: boolean = Array.isArray(data) || data.length < 1 || !data;
+        setDataEmpty(empty);
+        setData(data)
+  
+      } catch (error) {
+        console.error(error)
+      }
+    }, [])
+
+    return {isDataEmpty,data}
 }
 export {useHome}
