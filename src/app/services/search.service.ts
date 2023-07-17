23f3d153e8ef457e5e653211ae85f85e78e190af
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-17 09:25:44 -0500

    Modified search.service.ts file: changed search method with new param sent a header http.

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,11 +1,25 @@
 import {SearchController} from '@/controllers'
 import { AxiosResponse } from 'axios';
+import {Http} from './http/http'
 
 class SearchService implements  SearchController{
+    private static service: SearchService;
+    private http: Http = new Http();
+    
 
-    search = (search: string): Promise<AxiosResponse<any, any>> => {
-        throw new Error('Method not implemented.');
+    search = async (params: URLSearchParams): Promise<AxiosResponse<any, any>> => {
+        return await this.http.get<any>(
+            `${process.env.REACT_CARS_API_NINJAS}`,
+            this.http.header_get_params(params)
+          );
     }
+
+    public static getService():SearchService{
+        if(this.service === undefined) this.service = new SearchService();
+        return this.service;
+    }
+
+    private constructor(){}
     
 }
 
