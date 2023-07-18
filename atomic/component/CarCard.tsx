e9218a538828a6df800e828c0db3d941f624903f
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-18 09:14:42 -0500

    Created CarCard.tsx file.

---
+++ atomic/component/CarCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CarCard = () => {
+  return (
+    <div className='car-card-group' >
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {}
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export {CarCard}
