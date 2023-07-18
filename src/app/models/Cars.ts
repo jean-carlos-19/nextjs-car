ec866fa66e99bbc3679b75095fbd6ff44afaa5e4
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-18 11:46:29 -0500

    Created Cars file.

---
+++ src/app/models/Cars.ts
@@ -0,0 +1,51 @@
+class Cars {
+    private static cars:Cars;
+    
+  
+
+    public calculateCarRent = (city_mpg: number, year: number) => {
+        const basePricePerDay = 50; // Base rental price per day in dollars
+        const mileageFactor = 0.1; // Additional rate per mile driven
+        const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+        // Calculate additional rate based on mileage and age
+        const mileageRate = city_mpg * mileageFactor;
+        const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+        // Calculate total rental rate per day
+        const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+        return rentalRatePerDay.toFixed(0);
+    }
+
+    public updateSearchParams = (type: string, value: string) => {
+        // Get the current URL search params
+        const searchParams = new URLSearchParams(window.location.search);
+
+        // Set the specified search parameter to the given value
+        searchParams.set(type, value);
+
+        // Set the specified search parameter to the given value
+        const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+        return newPathname;
+    }
+    public deleteSearchParams = (type: string) => {
+        // Set the specified search parameter to the given value
+        const newSearchParams = new URLSearchParams(window.location.search);
+
+        // Delete the specified search parameter
+        newSearchParams.delete(type.toLocaleLowerCase());
+
+        // Construct the updated URL pathname with the deleted search parameter
+        const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+
+        return newPathname;
+    }
+    public static get_cars = ():Cars=>{
+        if(this.cars === undefined) this.cars = new Cars();
+        return this.cars;
+    }
+    private constructor() { }
+}
+export { Cars }
