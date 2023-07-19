9f4d15f96e91326cee36591bbefb9fdc0b36b595
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-19 12:19:15 -0500

    Modified index.ts file from element folder. Imported SearchButton element.

--- atomic/element/index.ts
+++ atomic/element/index.ts
@@ -1,3 +1,4 @@
 export { CustomButton } from "./CustomButton";
 export { CustomFilter } from "./CustomFilter";
 export { SearchManuFacturer } from "./SearchManuFacturer";
+export {SearchButton} from "./SearchButton";
