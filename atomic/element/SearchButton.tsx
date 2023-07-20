4526944b05a3491c9a547a37777340ac99ae5590
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-20 10:07:41 -0500

    Modified SearchButton file: build the component SearchButtton.

--- atomic/element/SearchButton.tsx
+++ atomic/element/SearchButton.tsx
@@ -1,10 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
+import { SearchButtonProps } from '@/types'
 
-const SearchButton = () => {
+const SearchButton = (props:SearchButtonProps) => {
+  const {otherClass,handleSubmit} = props;
   return (
-    <div>
-      
-    </div>
+    <button
+        onClick={handleSubmit}
+        type='button'
+        className={`-ml-3 z-10 ${otherClass}`}
+    >
+      <Image 
+        src={"/magnifying-glass.svg"}
+        alt='maginifying glass'
+        width={40}
+        height={40}
+        className='object-contain'
+      />
+    </button>
   )
 }
 
