dfbe9e05364e7e0150c944886ac4baf5ccf1d129
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-26 16:02:08 -0500

    Modfied next.config.js file: added the variable virtual.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        REACT_CARS_API_NINJAS: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-        REACT_X_RAPIDAPI_KEY: '4c1fb54fd3mshb7dd5e1244d446fp18d46djsn8cb9881192a3',
-        REACT_X_RAPIDAPI_HOST: 'cars-by-api-ninjas.p.rapidapi.com'
+        REACT_CARS_API_NINJAS: process.env.REACT_CARS_API_NINJAS,
+        REACT_X_RAPIDAPI_KEY: process.env.REACT_X_RAPIDAPI_KEY,
+        REACT_X_RAPIDAPI_HOST: process.env.REACT_X_RAPIDAPI_HOST
     },
     images: {
         remotePatterns:[
